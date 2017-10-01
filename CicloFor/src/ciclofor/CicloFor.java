34a78efdca32cbb85b8166ad38263b692adf9f82
Suricata <Suricata@192.168.1.74>
2017-09-30 20:23:04 -0500

    Suma de enteros haciendo uso de librería Scanner

---
+++ CicloFor/src/ciclofor/CicloFor.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ciclofor;
+
+/**
+ *
+ * @author Suricata
+ */
+public class CicloFor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        for(int contador =7;contador <=77;contador+=7){
+            
+            System.out.printf("%d", contador);
+            System.out.println();
+        }
+    }
+    
+}
